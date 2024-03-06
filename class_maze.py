bde7a1335c706172d523ec2e38c4887e4524fc6e
jnardone <jnardone@calpoly.edu>
2024-03-06 14:34:57 -0800

    Changed up DGN testing

--- class_maze.py
+++ class_maze.py
@@ -20,7 +20,6 @@ class Maze:
         self.goal_pc = mpimg.imread(goal_filepath)
         self.start_pt = start_pt
         self.goal_pt = goal_pt
-        # NOTE: Might not need self.traversed anymore, since class_DQN is taking care of the history/memorizing episodes
         self.traversed = []
         self.min_reward = -2*maze.size
         self.total_reward = 0
@@ -35,7 +34,7 @@ class Maze:
 
 
         marker = self.marker
-        marker = np.rot90(self.marker, k=self.robot_orientation) # k = 1 means rotate it 90 degrees CC
+        marker = np.rot90(self.marker, k=self.robot_orientation)
         imagebox = OffsetImage(marker, zoom = 0.20, cmap = 'gray')
 
 
@@ -59,19 +58,10 @@ class Maze:
         Returns:
             None
         """
-        # Read the input image
         image = cv.imread(image_path)
-
-        # Get the original dimensions
         original_height, original_width = image.shape[:2]
-
-        # Calculate the scaling factor to make the image square
         scale_factor = target_size / max(original_height, original_width)
-
-        # Resize the image to the target size
         resized_image = cv.resize(image, (int(original_width * scale_factor), int(original_height * scale_factor)))
-
-        # Save the resized image
         cv.imwrite(image_path, resized_image)
 
     def resize_image_to_square(self, image_path, target_size):
@@ -86,19 +76,10 @@ class Maze:
         Returns:
             None
         """
-        # Read the input image
         image = cv.imread(image_path)
-
-        # Get the original dimensions
         original_height, original_width = image.shape[:2]
-
-        # Calculate the scaling factor to make the image square
         scale_factor = target_size / max(original_height, original_width)
-
-        # Resize the image to the target size
         resized_image = cv.resize(image, (int(original_width * scale_factor), int(original_height * scale_factor)))
-
-        # Save the resized image
         cv.imwrite(image_path, resized_image)
 
     def generate_img(self, time_step):
@@ -113,45 +94,23 @@ class Maze:
         scaled_zoom =1/nrows
 
         marker = self.marker
-        marker = np.rot90(self.marker, k=self.robot_orientation)  # k = 1 means rotate it 90 degrees CC
-        # starbox = OffsetImage(self.goal_pc, zoom=scaled_zoom)
-        # goal = AnnotationBbox(starbox, (self.goal_pt[0], self.goal_pt[1]), frameon=False)
-        # ax.add_artist(goal)
+        marker = np.rot90(self.marker, k=self.robot_orientation)
         imagebox = OffsetImage(marker, zoom=scaled_zoom, cmap='gray')
         ab = AnnotationBbox(imagebox, (self.robot_location[0], self.robot_location[1]), frameon=False)
-        marker = np.rot90(self.marker, k=self.robot_orientation)  # k = 1 means rotate it 90 degrees CC
-        # starbox = OffsetImage(self.goal_pc, zoom=scaled_zoom)
-        # goal = AnnotationBbox(starbox, (self.goal_pt[0], self.goal_pt[1]), frameon=False)
-        # ax.add_artist(goal)
+        marker = np.rot90(self.marker, k=self.robot_orientation)
         imagebox = OffsetImage(marker, zoom=scaled_zoom, cmap='gray')
         ab = AnnotationBbox(imagebox, (self.robot_location[0], self.robot_location[1]), frameon=False)
         ax.add_artist(ab)
-
-        directory = 'robot_steps/'
-
         directory = 'robot_steps/'
         plt.imshow(self.maze, interpolation='none', cmap='binary')
         if not os.path.exists(directory):
             os.makedirs(directory)
         fig = plt.savefig(directory + str(time_step) + '.jpg', bbox_inches='tight')
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        fig = plt.savefig(directory + str(time_step) + '.jpg', bbox_inches='tight')
         plt.close(fig)
         image = cv.imread(directory + str(time_step) + '.jpg')
-        # self.resize_image_to_square(directory + str(time_step) + '.jpg', image.shape[0])
-
         cv.imshow('Frame', image)
-        cv.waitKey(50)
-        image = cv.imread(directory + str(time_step) + '.jpg')
-        # self.resize_image_to_square(directory + str(time_step) + '.jpg', image.shape[0])
-
-        cv.imshow('Frame', image)
-        cv.waitKey(50)
+        cv.waitKey(1)
         return image
-
-            
-
             
 
     def reset(self, time_step):
@@ -169,83 +128,56 @@ class Maze:
         for img_file in img_files:
            os.remove(img_file)
 
-
-        directory = 'robot_steps/'
-        img_files = glob.glob(os.path.join(directory, "*.[pjJ][npNP][gG]*"))
-        for img_file in img_files:
-           os.remove(img_file)
-
         cur_state_img = self.generate_img(time_step)
         return cur_state_img
 
     def move_robot(self, direction:str):
         robot_x, robot_y = self.robot_location[0], self.robot_location[1]
-        # TODO: Consider if I still need to append to a traversed location in line above, if robot does not move from invalid move.
         if direction == "UP":
             test_location = (robot_x, robot_y-1)
             expected_angle = 0
-            # Maze Edge Check
             if ((test_location[0]) < 0 or (test_location[1] < 0) or (test_location[0] > self.maze.shape[0]-1) or (test_location[1] > self.maze.shape[1]-1)):
-                # print ("ERROR: Maze Edge detected. Cannot traverse " + direction + ".")
                 raise ActionError("ERROR: Maze Edge detected. Cannot traverse " + direction + ".")
-            # Wall Check
             elif self.maze[test_location[1], test_location[0]] == 1:
-                # print ("ERROR: Wall detected. Cannot traverse " + direction + ".")
                 raise ActionError("ERROR: Wall detected. Cannot traverse " + direction + ".")
             else:
                 if (self.robot_orientation) != (expected_angle//90):
-                    # print("Rotating Robot")
                     self.robot_orientation = expected_angle//90
                 self.traversed.append((robot_x, robot_y))
                 self.robot_location = (robot_x, robot_y-1)
         elif direction == "DOWN":
             test_location = (robot_x, robot_y+1)
             expected_angle = 180
-            # Maze Edge Check
             if ((test_location[0]) < 0 or (test_location[1] < 0) or (test_location[0] > self.maze.shape[0]-1) or (test_location[1] > self.maze.shape[1]-1)):
-                # print ("ERROR: Maze Edge detected. Cannot traverse " + direction + ".")
                 raise ActionError("ERROR: Maze Edge detected. Cannot traverse " + direction + ".")
-            # Wall Check
             elif self.maze[test_location[1], test_location[0]] == 1:
-                # print ("ERROR: Wall detected. Cannot traverse " + direction + ".")
                 raise ActionError("ERROR: Wall detected. Cannot traverse " + direction + ".")
             else:
                 if (self.robot_orientation) != (expected_angle//90):
-                    # print("Rotating Robot")
                     self.robot_orientation = expected_angle//90
                 self.traversed.append((robot_x, robot_y))
                 self.robot_location = (robot_x, robot_y+1)
         elif direction == "LEFT":
             test_location = (robot_x-1, robot_y)
             expected_angle = 90
-            # Maze Edge Check
             if ((test_location[1]) < 0 or (test_location[0] < 0) or (test_location[0] > self.maze.shape[0]-1) or (test_location[1] > self.maze.shape[1]-1)):
-                # print ("ERROR: Maze Edge detected. Cannot traverse " + direction + ".")
                 raise ActionError("ERROR: Maze Edge detected. Cannot traverse " + direction + ".")
-            # Wall Check
             elif self.maze[test_location[1], test_location[0]] == 1:
-                # print ("ERROR: Wall detected. Cannot traverse " + direction + ".")
                 raise ActionError("ERROR: Wall detected. Cannot traverse " + direction + ".")
             else:
                 if (self.robot_orientation) != (expected_angle//90):
-                    # print("Rotating Robot")
                     self.robot_orientation = expected_angle//90
                 self.traversed.append((robot_x, robot_y))
                 self.robot_location = (robot_x-1, robot_y)
         elif direction == "RIGHT":
             test_location = (robot_x+1, robot_y)
             expected_angle = 270
-            # Maze Edge Check
             if ((test_location[1]) < 0 or (test_location[0] < 0) or (test_location[0] > self.maze.shape[0]-1) or (test_location[1] > self.maze.shape[1]-1)):
-                # print ("ERROR: Maze Edge detected. Cannot traverse " + direction + ".")
                 raise ActionError("ERROR: Maze Edge detected. Cannot traverse " + direction + ".")
-            # Wall Check
             elif self.maze[test_location[1], test_location[0]] == 1:
-                # print ("ERROR: Wall detected. Cannot traverse " + direction + ".")
                 raise ActionError("ERROR: Wall detected. Cannot traverse " + direction + ".")
             else:
                 if (self.robot_orientation) != (expected_angle//90):
-                    # print("Rotating Robot")
                     self.robot_orientation = expected_angle//90
                 self.traversed.append((robot_x, robot_y))
                 self.robot_location = (robot_x+1, robot_y)
@@ -253,7 +185,6 @@ class Maze:
     def get_available_actions(self):
         robot_x, robot_y = self.robot_location[0], self.robot_location[1]
         valid_actions = []
-        # valid_actions = ["UP", "DOWN", "LEFT", "RIGHT"]
 
         # Testing UP action:
         up_location = (robot_x, robot_y-1)
@@ -313,10 +244,10 @@ class Maze:
             return 1
         # Robot has already visited this spot
         if (robot_x, robot_y) in self.traversed:
-            return -0.04
+            return -0.5
         else:
             # Advanced onto a new spot in the maze, but hasn't reached the goal or gone backwards
-            return 0.08
+            return -0.25
     
     def game_over(self):
         robot_x, robot_y = self.robot_location[0], self.robot_location[1]
@@ -331,8 +262,6 @@ class Maze:
 
     def take_action(self, action: str, time_step):
         self.move_robot(action)
-        self.total_reward += self.get_reward()
-        return (self.generate_img(time_step), self.get_reward(), self.game_over())
         self.total_reward += self.get_reward()
         return (self.generate_img(time_step), self.get_reward(), self.game_over())
 
