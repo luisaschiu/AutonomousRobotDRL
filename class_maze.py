e23564d1324f0b8b327af6982ac3d2abbf03dc3d
jnardone <jnardone@calpoly.edu>
2024-03-06 12:37:45 -0800

    Some minor tweaks.

--- class_maze.py
+++ class_maze.py
@@ -316,13 +316,13 @@ class Maze:
         robot_x, robot_y = self.robot_location[0], self.robot_location[1]
         # Robot reached the goal
         if robot_x == self.goal_pt[0] and robot_y == self.goal_pt[1]:
-            return 5
+            return 1
         # Robot has already visited this spot
         if (robot_x, robot_y) in self.traversed:
-            return -2
+            return -0.04
         else:
             # Advanced onto a new spot in the maze, but hasn't reached the goal or gone backwards
-            return 1
+            return 0.08
     
     def game_over(self):
         robot_x, robot_y = self.robot_location[0], self.robot_location[1]
